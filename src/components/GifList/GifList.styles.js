2fba12bacb20378a5e2d60ed05b9ea175af7d928
Eleazar Maestre <eleazarenrique23@gmail.com>
2021-07-13 21:03:42 -0400

    Improve styling of gif list

---
+++ src/components/GifList/GifList.styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+    favoriteIcon: {
+        color: red[500],
+    },
+}));
+
+export default useStyles;

4609a55b5d585d0c2223f55806bc5563624eed48
Eleazar Maestre <eleazarenrique23@gmail.com>
2021-07-13 21:04:28 -0400

    Add support of like gifs

--- src/components/GifList/GifList.js
+++ src/components/GifList/GifList.js
@@ -1,21 +1,29 @@
-import React, { useMemo } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+import React, { Fragment, useCallback, useMemo, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
 import {
+    CircularProgress,
     ImageList,
     ImageListItem,
     ImageListItemBar,
     IconButton,
+    Snackbar,
+    Tooltip,
     Typography,
 } from '@material-ui/core';
-import { Info as InfoIcon } from '@material-ui/icons';
+import { Alert } from '@material-ui/lab';
+import { FavoriteBorder as FavoriteBorderIcon } from '@material-ui/icons';
 
-// import { like, selectLikedGifs, unlike } from 'reducers/likedGifsSlice';
+import { like } from 'reducers/likedGifsSlice';
 import { useGetGifsByTermQuery } from 'services/giphy';
+import useStyles from './GifList.styles';
 
 function GifList(props) {
-    const { termToSearch } = props;
-    // const likedGifs = useSelector(selectLikedGifs);
-    // const dispatch = useDispatch();
+    const { termToSearch, ...restProps } = props;
+    const dispatch = useDispatch();
+    const classes = useStyles();
+
+    const [open, setOpen] = useState(false);
 
     const {
         data: response,
@@ -28,36 +36,79 @@ function GifList(props) {
         [response]
     );
 
+    const handlerClick = useCallback(
+        (gif) => () => {
+            dispatch(like(gif));
+            setOpen(true);
+        },
+        [dispatch]
+    );
+
+    const handleClose = useCallback((event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    }, []);
+
     if (error) {
         return <Typography>Oh no, there was an error.</Typography>;
     }
 
     if (isLoading) {
-        return <Typography>Loading...</Typography>;
+        return <CircularProgress />;
     }
 
     return (
-        <div>
-            <ImageList cols={4}>
+        <Fragment>
+            <Snackbar
+                open={open}
+                autoHideDuration={6000}
+                onClose={handleClose}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+            >
+                <Alert
+                    variant="filled"
+                    severity="success"
+                    elevation={6}
+                    onClose={handleClose}
+                >
+                    GIF liked!
+                </Alert>
+            </Snackbar>
+            <ImageList cols={4} {...restProps}>
                 {fetchedGifs.map((gif) => (
                     <ImageListItem key={gif.id}>
                         <img src={gif.images.downsized.url} alt={gif.title} />
                         <ImageListItemBar
                             title={gif.title}
-                            subtitle={<span>by: {gif.username}</span>}
+                            subtitle={
+                                <span>
+                                    by:{' '}
+                                    {gif.username ? gif.username : 'anonymous'}
+                                </span>
+                            }
                             actionIcon={
-                                <IconButton
-                                    aria-label={`info about ${gif.title}`}
-                                >
-                                    <InfoIcon />
-                                </IconButton>
+                                <Tooltip title="Add this gif to your favorites">
+                                    <IconButton
+                                        className={classes.favoriteIcon}
+                                        onClick={handlerClick(gif)}
+                                    >
+                                        <FavoriteBorderIcon />
+                                    </IconButton>
+                                </Tooltip>
                             }
                         />
                     </ImageListItem>
                 ))}
             </ImageList>
-        </div>
+        </Fragment>
     );
 }
+
+GifList.propTypes = {
+    termToSearch: PropTypes.string.isRequired,
+};
 
 export default GifList;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import App from './App';
+import GifFinder from './GifFinder';
 import GifList from './GifList';
+import LikedGifs from './LikedGifs';
 import SearchBar from './SearchBar';
 
-export { App, GifList, SearchBar };
+export { App, GifFinder, GifList, LikedGifs, SearchBar };
